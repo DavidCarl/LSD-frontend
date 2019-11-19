af9e831febca3bc309e34a1cfef4769fbf002a44
Mikkel <MikkelHansen95@live.dk>
2019-11-19 23:34:06 +0100

    login servlet

--- src/main/java/com/zee/servlets/LoginServlet.java
+++ src/main/java/com/zee/servlets/LoginServlet.java
@@ -8,23 +8,28 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
+import classes.User;
+
 
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
-    /*
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, java.io.IOException {
         try {
-            String usernameInput = request.getParameter("un");
+            String usernameInput = request.getParameter("user");
             String passwordInput = request.getParameter("pw");
-            if (usernameInput != null && usernameInput.equals("Mo") && passwordInput.equals("123")) {
-                HttpSession session = request.getSession();
-                session.setAttribute("currentSessionUser", usernameInput);
-                //System.out.println("who logged in ? :"+session.getAttribute("currentSessionUser"));;
-                response.sendRedirect("start.jsp");
+            if (usernameInput != null) {
+                System.out.println("ses");
+                //User user = new User(1,105,usernameInput,passwordInput);
+                System.out.println("med");
+                //System.out.println(user.toString());
+                //HttpSession session = request.getSession();
+                //session.setAttribute("currentSessionUser", user);
+                //System.out.println("who logged in ? :"+session.getAttribute("currentSessionUser"));
+                response.sendRedirect("home.jsp");
             } else {
-                response.sendRedirect("flightDeals.jsp"); //error page
+                response.sendRedirect("fuckoff.jsp"); //error page
             }
         } catch (Throwable theException) {
             System.out.println(theException);
