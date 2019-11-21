4789a7d12353c75560f36adbf68fa90356ea90f9
unknown <marik.3000@hotmail.com>
2019-11-21 14:08:18 +0100

    new

--- src/main/java/com/zee/servlets/TestContract.java
+++ src/main/java/com/zee/servlets/TestContract.java
@@ -1,19 +1,61 @@
 package com.zee.servlets;
 
-import contract.dto.*;
+
+
+import contract.dto.Flight;
 import contract.dto.FlightOffer;
+import contract.dto.User;
+import contract.interfaces.BeanInterface;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.Remote;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 
-import java.util.*;
-
+@Remote
 public class TestContract {
 
-    public static void main(String[] args) {
+    private static InitialContext ic;
+    //private String host="",port="";
 
-        //     Collection<Offers> getFlightOffers(Date start, Date end, String depIata, String destIata, boolean oneWay);
-     Offers offer = new Offers();
-     offer.getFlightOffers(new Date(), new Date(),"", "", false );
+    public void loadProperties(String h, String p) {
+        try {
+            System.out.println("h: " + h + " p: " + p);
+
+            Properties prop = new Properties();
+            prop.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+            prop.put(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.naming.remote.client.InitialContextFactory");
+            prop.put(Context.PROVIDER_URL, "http-remoting://localhost:8080"); //backend.dcarl.me
+
+            prop.put("jboss.naming.client.ejb.context", false);
+
+            ic = new InitialContext(prop);
+        } catch (NamingException ex) {
+            Logger.getLogger(TestContract.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void main(String a[]) {
+
+        try {
+            final String appName = "https://www.backend.dcarl.me";
+            final String moduleName = "spring-ejb-remote";
+            final String distinctName = "";
+            final String beanName = "Backend";
+            final String viewClassName = BeanInterface.class.getName();
+            final String toLookup = String.format("ejb:%s/%s/%s/%s!%s", appName, moduleName, distinctName, beanName, viewClassName);
+            new TestContract().loadProperties("https://www.backend.dcarl.me", "80");
+            BeanInterface etr = (BeanInterface) ic.lookup("ejb:/backend/ContractBean!contract.interfaces.BeanInterface"); //ejb:/www.backend.me//ContractBean!
+          System.out.println(etr.whoAmI("David"));
 
 
 
+        } catch (NamingException ex) {
+            System.out.println(ex);
+        }
     }
 }
